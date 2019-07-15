fa06a50b6631765d9fa68f6c71b8d08e1ae6ff16
Gabriel <gabriel_viciosobarrera@hotmail.com>
2019-07-15 16:20:10 -0400

    added basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import Home from  './pages/Home'
 import Hero from  './pages/Hero'
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import Navbar from './pages/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/hero/:id' component={Hero}/>
