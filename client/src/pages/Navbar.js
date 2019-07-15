b07588eb98794173f01b4dbdad0eeadb44f97c9f
Gabriel <gabriel_viciosobarrera@hotmail.com>
2019-07-15 11:23:51 -0400

    basic navbar

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => (
   <nav>
     <ul>
       <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink exact to="/movies">Heros</NavLink></li>
-      <li><NavLink exact to="/movies">About Us</NavLink></li>
-      <li><NavLink exact to="/movies">Details</NavLink></li>
+      <li><NavLink exact to="/">Heros</NavLink></li>
+      <li><NavLink exact to="/">About Us</NavLink></li>
+      <li><NavLink exact to="/">Details</NavLink></li>
     </ul>
   </nav>
 )
