e07ef15fe494637f303fe053aa7d417bb535d4b6
Andy Weiss <andyweiss1982@gmail.com>
2019-07-18 10:31:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const axios = require('axios')
 app.get('/superhero/:hero', (request,response)=> {
   console.log('hit');
   axios.get(`https://superheroapi.com/api/${process.env.SUPER_HERO_KEY}/search/${request.params.hero}`)
-  .then(superHero => response.json(superHero.data.results) || [])
+  .then(superHero => response.json(superHero.data.results || []))
   .catch( error => response.json({error: "There are no Heroes with that name try again"}))
 })
 
 app.get('/hero/:id', (request,response)=> {
   console.log('hey buddy');
   axios.get(`https://superheroapi.com/api/${process.env.SUPER_HERO_KEY}/${request.params.id}`)
-  .then(hero => response.json(hero.data) || [])
+  .then(hero => response.json(hero.data || {}))
   .catch(error => console.log("error", error))
 })
 
