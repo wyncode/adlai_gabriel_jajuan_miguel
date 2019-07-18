a7bfe419d22334cd40b3215285b2fc71341a7361
Andy Weiss <andyweiss1982@gmail.com>
2019-07-18 10:08:40 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/yourheroes' component={SelectedHero}/>
-        <Route exact path='/hero/:id' component={Hero}/>
+        <Route path='/hero/:id' component={Hero}/>
       </Switch>
     </BrowserRouter>
   );
