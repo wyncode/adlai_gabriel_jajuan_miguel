da6169bba61f4955538774ace64931533899da56
Andy Weiss <andyweiss1982@gmail.com>
2019-07-18 10:09:08 -0400

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => (
     <nav>
       <ul>
         <li><NavLink exact to="/">Home</NavLink></li>
-        <li><NavLink exact to="/">Selected Heros</NavLink></li>
+        <li><NavLink exact to="/yourheroes">Selected Heros</NavLink></li>
       </ul>
     </nav>
     <section className="seperator-wrapper">
